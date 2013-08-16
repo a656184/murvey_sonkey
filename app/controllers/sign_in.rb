e3d3fca56087386e57e5db9a3f0113de74f19035
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-08-16 12:10:56 -0700

    sign-in routes and session

---
+++ app/controllers/sign_in.rb
@@ -0,0 +1,27 @@
+get '/login' do
+  erb :login_form
+end
+
+post '/login' do
+  user = User.authenticate(params[:name], params[:password])
+  if user
+    session[:user_id] = user.id
+  else
+    session[:user_id] = nil
+  end
+  redirect '/'
+end
+
+post '/sign_up' do
+  user = User.new(name: params[:name], email: params[:email])
+  user.password = params[:password]
+  user.save
+  # THIS NEEDS TO BE FIXED SO THE USER IS ONLY LOGGED IN IF THE VALIDATIONS PASS
+  session[:user_id] = user.id
+  redirect '/'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
