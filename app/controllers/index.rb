707e19282ffc50b29a81a02a2c1c3923ba42762a
DBC Apprentice <apprentice@devbootcamp.com>
2013-08-16 12:18:33 -0700

    Create taking survey and creating survey routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,28 @@
-get '/' do
+get '/user/:user_id' do
   # Look in app/views/index.erb
-  erb :index
+  erb :profile
+end
+
+# ------- Creator of Survey ------
+
+get '/survey/new'do
+  # display new survey form
+  erb :create_survey
+end
+
+post '/survey' do
+  # submit info for new survey
+  redirect '/user/:user_id'
+end
+
+# ------- Taker of Survey ------
+
+get '/survey/:id' do
+  # display survey to take
+  erb :take_survey
+end
+
+post '/survey/:id' do
+  # submit info for survey taken
+  redirect '/user/:user_id'
 end
