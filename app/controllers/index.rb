451f942cad6b6b6204e137accaee2b6ed53b3177
DBC Apprentice <apprentice@devbootcamp.com>
2013-08-16 17:11:46 -0700

    change take survey logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,36 +16,30 @@ get '/survey/new'do
   erb :create_survey
 end
 
+#submit info for new survey
 post '/survey' do
-  # p params
+
   @survey = Survey.create(title: params[:title], user_id: session[:user_id] )
-  # p params[:choice]
-
   @question = Question.create(survey_id: @survey.id, prompt: params[:prompt])
-
   Choice.create(question_id: @question.id, option: params[:choice])
-
-  #need survey title and user id in params to create survey
-  # submit info for new survey
   redirect '/user'
 end
 
 # ------- Taker of Survey ------
 
+#display survey to take
 get '/survey/:id' do
+
   @survey = Survey.find(params[:id])
-
   @question = Question.where(survey_id: @survey.id)
-
   @choice = Choice.where(question_id: @question[0].id)
-  # display survey to take
   erb :take_survey
 end
 
+#submit info for survey taken
 post '/survey/:id' do
-  p params
+
   @survey_taker = SurveyTaker.create(survey_id: params[:id], user_id: session[:user_id])
   @vote = Vote.create(user_id: session[:user_id], choice_id: params[:choice_id], question_id: params[:question_id], survey_taker_id: @survey_taker.id)
-  # submit info for survey taken
   redirect '/user'
 end
