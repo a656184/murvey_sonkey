24e00ca184073c135e4ae9d0c2211e220c2287c2
mehagel65@gmail.com <mehagel65@gmail.com>
2013-08-17 12:24:31 -0700

    JQuery on create survey

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+
+
+
+	$("#add_choice").on('click', function(e){
+		e.preventDefault();
+		console.log('hello world');
+	})
+
+
+		$("#add_question").on('click', function(e){
+		e.preventDefault();
+		console.log('goodbye world');
+	})
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
